3d99367b8d62ed4e384fd2de31ac104a1fd8c811
javgg <77599168+javgg@users.noreply.github.com>
2021-04-15 14:16:56 +0800

    Create jav.py

---
+++ javgg.net/jav.py
@@ -0,0 +1,5 @@
+Watch jav online at https://javgg.net
+  
+  *Acknowledgements*
+
+This project does not include any stored or static data, and all the data it presents are collected realtime and automatically from the websites below. Internet data are public but messy and collecting them is a tiring work. Appreciate them for their offering precious and high quality data.
